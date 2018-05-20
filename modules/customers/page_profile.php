4238fac9d492b33e2fe0b67604675f975de10a4f
Khazefa IV <cybergitt@gmail.com>
2018-05-20 19:03:36 +0700

    Add front module profile

---
+++ modules/customers/page_profile.php
@@ -0,0 +1,13 @@
+<?php
+    $pagetitle = "Edit Akun";
+?>
+
+<div id="heading-breadcrumbs">
+  <div class="container">
+    <div class="row d-flex align-items-center flex-wrap">
+      <div class="col-md-12">
+        <h1 class="h2"><?php echo $pagetitle; ?></h1>
+      </div>
+    </div>
+  </div>
+</div>
